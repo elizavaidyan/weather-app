929d935023433977b63013e4d6c05304fe128559
elizavaidyan <65553237+elizavaidyan@users.noreply.github.com>
2020-12-17 21:48:17 +0530

    HTTP requests with Fetch

--- web-server/public/js/app1.js
+++ web-server/public/js/app1.js
@@ -1,1 +1,15 @@
-console.log("Loading client side javascript")
+const { response } = require("express");
+
+console.log("Loading client side javascript");
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     })
+// })
+
+// const weatherForm = document.querySelector('form');
+
+// weatherForm.addEventListener('submit', () => {
+//     console.log('Listening');
+// })
