b82cf32c60b22f512f9d127af13037c0bc819aef
elizavaidyan <65553237+elizavaidyan@users.noreply.github.com>
2020-12-18 18:29:01 +0530

    Browser HTTP requests with fetch- modified

--- web-server/public/js/app1.js
+++ web-server/public/js/app1.js
@@ -1,15 +1,32 @@
-const { response } = require("express");
 
 console.log("Loading client side javascript");
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    })
+})
+
+fetch('http://localhost:3000/weather?address=India').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+
+    })
+})
 
 // const weatherForm = document.querySelector('form');
+// const search = document.querySelector('input');
 
-// weatherForm.addEventListener('submit', () => {
-//     console.log('Listening');
+// weatherForm.addEventListener('submit', (e) => {
+
+//     e.preventDefault();
+
+//     const location = search.value;
+//     //console.log('Listening');
+//     console.log(location);
 // })
